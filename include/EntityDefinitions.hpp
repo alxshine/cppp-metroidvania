e5b1cfdbd741c0292451676db378bedf205a6805
Bennett Piater <bennett@piater.name>
2018-10-18 17:54:51 +0200

    add operator<< to the definition classes

--- include/EntityDefinitions.hpp
+++ include/EntityDefinitions.hpp
@@ -3,12 +3,12 @@
 
 #include <chrono>
 #include <optional>
+#include <ostream>
 #include <vector>
 
 /**********************************************
-*  Classes to parse our config into memory.  *
-**********************************************/
-
+ *  Classes to parse our config into memory.  *
+ **********************************************/
 
 namespace game_definitions {
 
@@ -38,6 +38,37 @@ struct Mob {
 	Animation idleAnimation;
 	std::vector<Attack> attacks;
 };
+
+/*********************
+ *  Print operators  *
+ *********************/
+
+inline std::ostream &operator<<(std::ostream &out, const Rectangle &rect)
+{
+	return out << "Rectangle {" << rect.topLeft << "," << rect.topRight << "," << rect.bottomLeft << ","
+	           << rect.bottomRight << "}";
+}
+
+inline std::ostream &operator<<(std::ostream &out, const Animation &anim)
+{
+	out << "Animation { Spritesheet: " << anim.spritesheet << "; Time: " << anim.timePerFrame.count() << "; Frames: \n";
+	for (auto f : anim.frames) {
+		out << "\t" << f << "\n";
+	}
+
+	return out << "\t}\n";
+}
+
+inline std::ostream &operator<<(std::ostream &out, const Mob mob)
+{
+	out << "Mob " << mob.name << " {\n";
+	out << "Health: " << mob.health << "\n";
+	out << "WalkingAnimation: " << mob.walkingAnimation;
+
+	if (mob.idleAnimation.spritesheet != "")
+		out << "IdleAnimation: " << mob.idleAnimation;
+	return out << "}\n";
+}
 
 } // namespace game_definitions
 
--- src/parsertest.cpp
+++ src/parsertest.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	Mob mob;
 	in >> mob;
 
-	std::cout << mob.name << " " << mob.health << "\n";
+	std::cout << mob << std::endl;
 
 	return 0;
 }
