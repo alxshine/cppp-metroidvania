02f04cc18c26f0ee6428f049b58131d1310d14db
Alex <alexander.schloegl@uibk.ac.at>
2019-01-17 10:26:44 +0100

    Mirrored slime spritesheet and fixed standingAI

--- src/game/AI.cpp
+++ src/game/AI.cpp
@@ -11,12 +11,12 @@ void StandingAI::controlEntity(Movable &movable, Attackable &attackable, const C
 	// find attacks in range
 	for (unsigned i = 0; i < attackable.attacks.size(); ++i) {
 		auto attackHitbox = attackable.getHitbox(movable.getPosition(), i, movable.getDirection().x < 0);
-  		// if (intersects(attackHitbox, playerHitbox)) {
+		if (intersects(attackHitbox, playerHitbox)) {
 			attackable.attack(i);
-      // std::cout << "using attack " << i << std::endl;
-			// break;
-		// }else{
-      // std::cout << "attack not in range" << std::endl;;
-    // }
+			std::cout << "using attack " << i << std::endl;
+			break;
+		} else {
+			std::cout << "attack not in range" << std::endl;
+		}
 	}
 }
