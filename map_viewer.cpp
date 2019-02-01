9694a685275c35daa5aa44d0a1153ed37f148f80
Bennett Piater <bennett@piater.name>
2019-02-01 23:16:09 +0100

    live-update map viewer in background!

--- map_viewer.cpp
+++ map_viewer.cpp
@@ -16,39 +16,41 @@ int main(int, char **argv)
 	std::string roomName = argv[1];
 	ResourceManager res{"../game_definitions", "../assets"};
 
-	// allow reloading assets
 	EventHandler events;
 	bool run = true;
-	bool error = false;
 	events.on(SDL_QUIT, [&run](const Event &) { run = false; });
-	events.onKeyDown(SDLK_r, [&res, &error](const KeyboardEvent &) {
-		try {
-			error = true;
-			res = {"../game_definitions", "../assets"};
-			error = false;
-		} catch (SdlException &e) {
-			cerr << e.what() << endl;
-		} catch (GameException &e) {
-			cerr << e.what() << endl;
-		} catch (std::runtime_error &e) {
-			cerr << e.what() << endl;
-		} catch (const string &e) {
-			cerr << e << endl;
-		}
-	});
 	Position cam{0, 0};
 	events.whileKeyHeld(SDL_SCANCODE_W, [&cam]() { cam.y -= 5; });
 	events.whileKeyHeld(SDL_SCANCODE_A, [&cam]() { cam.x -= 5; });
 	events.whileKeyHeld(SDL_SCANCODE_S, [&cam]() { cam.y += 5; });
 	events.whileKeyHeld(SDL_SCANCODE_D, [&cam]() { cam.x += 5; });
-	events.onKeyDown(SDLK_SPACE, [&cam](const KeyboardEvent &) { cam = {0, 0}; });
+	events.onKeyDown(SDLK_r, [&cam](const KeyboardEvent &) { cam = {0, 0}; });
 
 	auto renderer(SDL::getInstance().getRenderer());
 
+	bool error = false;
+	int i = 0;
 	while (run) {
+		i++;
 		renderer.clear();
 		renderer.setCameraPosition(cam);
 		events.dispatch();
+
+		if (i == 60)
+			try {
+				i = 0;
+				error = true;
+				res = {"../game_definitions", "../assets"};
+				error = false;
+			} catch (SdlException &e) {
+				cerr << e.what() << endl;
+			} catch (GameException &e) {
+				cerr << e.what() << endl;
+			} catch (std::runtime_error &e) {
+				cerr << e.what() << endl;
+			} catch (const string &e) {
+				cerr << e << endl;
+			}
 
 		if (!error) {
 			Room room(res.getRoom(roomName));
